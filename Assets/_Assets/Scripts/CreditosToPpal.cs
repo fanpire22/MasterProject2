11b4545c1c1702644c4e93e6b569182ef157c606
Fanpy <Fanpire22@yahoo.es>
2018-04-03 23:10:29 +0200

    SFX

---
+++ Assets/_Assets/Scripts/CreditosToPpal.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditosToPpal : MonoBehaviour
+{
+
+    [SerializeField] private AudioClip _sfxCICE;
+    [SerializeField] private AudioClip _menuBGM;
+    [SerializeField] private GameObject _titulo;
+    [SerializeField] private GameObject _creditos;
+
+    private AudioSource _audio;
+
+    private void Awake()
+    {
+        _audio = gameObject.GetComponent<AudioSource>();
+    }
+
+    private void Start()
+    {
+        _audio.PlayOneShot(_sfxCICE);
+    }
+
+    private void Update()
+    {
+        if (!_audio.isPlaying)
+        {
+            _creditos.SetActive(false);
+            _titulo.SetActive(true);
+            _audio.clip = _menuBGM;
+            _audio.loop = true;
+            _audio.Play();
+
+            gameObject.GetComponent<CreditosToPpal>().enabled=false;
+        }
+    }
+}
